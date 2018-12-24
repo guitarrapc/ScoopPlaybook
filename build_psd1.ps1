ccf5f18097fb85227cd47b910e6dfbfe85f186f2
Ikiru Yoshizaki <yoshizaki@nextscape.net>
2018-12-25 04:04:30 +0900

    fix

--- build_psd1.ps1
+++ build_psd1.ps1
@@ -10,6 +10,18 @@ $guid = "38ac9351-b293-4166-9ae5-13dd349d6ad6"
 $publish = "./publish/ScoopPlaybook"
 $targets = "ScoopPlaybook.ps*1"
 
+# setup
+function Update([string]$Path, [string]$Version, [string]$Guid) {
+    New-ModuleManifest -Path $Path -Guid $Guid -PowerShellVersion 5.1 -Author guitarrapc -ModuleVersion $Version -RootModule ScoopPlaybook.psm1 -Description "PowerShell Module to run scoop like ansible playbook" -CompatiblePSEditions Core, Desktop -Tags UTF8BOM -ProjectUri https://github.com/guitarrapc/ScoopPlaybook -LicenseUri https://github.com/guitarrapc/ScoopPlaybook/blob/master/LICENSE.md -ReleaseNotes https://github.com/guitarrapc/ScoopPlaybook/releases/tag/$Version
+}
+
+function Prepare([string]$Path) {
+    if (Test-Path $Path) {
+        Remove-Item $Path -Force -Recurse
+    }
+    New-Item -Path $Path -ItemType Directory -Force    
+}
+
 # validation
 if ([string]::IsNullOrWhiteSpace($Version)) {
     Write-Host -ForeGroundColor Yellow "Version not specified, please specify semantic version."
@@ -27,18 +39,6 @@ if (Test-Path $path) {
         Copy-Item -Path src/*, *.md -Destination "$publish/"
         return
     }
-}
-
-# setup
-function Update([string]$Path, [string]$Version, [string]$Guid) {
-    New-ModuleManifest -Path $Path -Guid $Guid -PowerShellVersion 5.1 -Author guitarrapc -ModuleVersion $Version -RootModule ScoopPlaybook.psm1 -Description "PowerShell Module to run scoop like ansible playbook" -CompatiblePSEditions Core, Desktop -Tags UTF8BOM -ProjectUri https://github.com/guitarrapc/ScoopPlaybook -LicenseUri https://github.com/guitarrapc/ScoopPlaybook/blob/master/LICENSE.md -ReleaseNotes https://github.com/guitarrapc/ScoopPlaybook/releases/tag/$Version
-}
-
-function Prepare([string]$Path) {
-    if (Test-Path $Path) {
-        Remove-Item $Path -Force -Recurse
-    }
-    New-Item -Path $Path -ItemType Directory -Force    
 }
 
 # run
