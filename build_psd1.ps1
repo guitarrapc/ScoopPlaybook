f483ede6b3e680e0c9a80ac5cd04a52b8f5c49de
Ikiru Yoshizaki <yoshizaki@nextscape.net>
2018-12-25 04:23:08 +0900

    [ci skip]

--- build_psd1.ps1
+++ build_psd1.ps1
@@ -8,11 +8,24 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $path = "$here/src/ScoopPlaybook.psd1"
 $guid = "38ac9351-b293-4166-9ae5-13dd349d6ad6"
 $publish = "./publish/ScoopPlaybook"
-$targets = "ScoopPlaybook.ps*1"
 
 # setup
 function Update([string]$Path, [string]$Version, [string]$Guid) {
-    New-ModuleManifest -Path $Path -Guid $Guid -PowerShellVersion 5.1 -Author guitarrapc -ModuleVersion $Version -RootModule ScoopPlaybook.psm1 -Description "PowerShell Module to run scoop like ansible playbook" -CompatiblePSEditions Core, Desktop -Tags UTF8BOM -ProjectUri https://github.com/guitarrapc/ScoopPlaybook -LicenseUri https://github.com/guitarrapc/ScoopPlaybook/blob/master/LICENSE.md -ReleaseNotes https://github.com/guitarrapc/ScoopPlaybook/releases/tag/$Version
+    $params = @{
+        Path                 = $Path
+        Guid                 = $Guid
+        PowerShellVersion    = "5.1"
+        Author               = "guitarrapc"
+        ModuleVersion        = $Version
+        RootModule           = "ScoopPlaybook.psm1"
+        Description          = "PowerShell Module to run scoop like ansible playbook"
+        CompatiblePSEditions = ("Core", "Desktop")
+        Tags                 = "Scoop"
+        ProjectUri           = "https://github.com/guitarrapc/ScoopPlaybook"
+        LicenseUri           = "https://github.com/guitarrapc/ScoopPlaybook/blob/master/LICENSE.md"
+        ReleaseNotes         = "https://github.com/guitarrapc/ScoopPlaybook/releases/tag/$Version"
+    }
+    New-ModuleManifest @params
 }
 
 function Prepare([string]$Path) {
