c3bd1e080861d0cda719d71ca1a21964388143c5
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-09-04 04:41:14 +0900

    chore: release script

--- publish_on_docker.ps1
+++ publish_on_docker.ps1
@@ -1,1 +1,1 @@
-docker run --rm -v ${pwd}:/app -w /app mcr.microsoft.com/powershell:lts-ubuntu-18.04 pwsh -file release.ps1 -Version 1.0.0
+docker run --rm -v ${pwd}:/app -w /app mcr.microsoft.com/powershell:lts-ubuntu-18.04 pwsh -file publish.ps1 -Version 1.0.0
